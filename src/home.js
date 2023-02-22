6b3246e4e2222b055d6021adb2c8501a212cabf0
Nicholas Licata <nicholas.licata730@gmail.com>
2023-02-21 21:08:21 -0500

    initial upload

---
+++ src/home.js
@@ -0,0 +1,30 @@
+
+function createHome() {
+    const homeContainer = document.createElement('div');
+    homeContainer.classList.add('home-container');
+
+    const homeHeaderContainer = document.createElement('div');
+    homeHeaderContainer.classList.add('home-header-container');
+    homeContainer.appendChild(homeHeaderContainer);
+
+    const homeHeader = document.createElement('h1');
+    homeHeader.classList.add('home-header');
+    homeHeader.textContent = 'Greetings Tarnished';
+    homeHeaderContainer.appendChild(homeHeader);
+
+    const homeP1 = document.createElement('p');
+    homeP1.classList.add('home-p');
+    homeP1.textContent = 'Welcome to the Elden Ring Compendium. Our goal is to provide the Tarnished of the Lands Between with a referential tool to assist in their journey to become Elden Lords.';
+    homeHeaderContainer.appendChild(homeP1);
+
+    return homeContainer;
+}
+
+function renderHome() {
+    const main = document.querySelector('main');
+    main.textContent = '';
+
+    main.appendChild(createHome());
+}
+
+export default renderHome;
